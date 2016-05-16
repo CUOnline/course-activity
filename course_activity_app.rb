32f7ecffdd2c03af4a4c955b9a155d1e3a37d31b
Connor Ford <connor.ford@ucdenver.edu>
2016-05-16 13:01:54 -0600

    Add data retrieval script

--- course_activity_app.rb
+++ course_activity_app.rb
@@ -6,6 +6,11 @@ class CourseActivityApp < WolfCore::App
   set :root, File.dirname(__FILE__)
   self.setup
 
-  get '/' do
+  post '/' do
+  end
+
+  get '/access-report.js' do
+    content_type 'application/javascript'
+    erb 'access-report.js'.to_sym
   end
 end
