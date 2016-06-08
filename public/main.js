211f9a9dcea2827b9f8d88ed016638e45ad7b45a
Connor Ford <connor.ford@ucdenver.edu>
2016-06-08 17:09:41 -0600

    Clean up CSS

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.category').click(function(e) {
     $(this).next().toggle('fade');
-
     var icon = $(this).find('span').first();
     icon.toggleClass('glyphicon-chevron-down');
     icon.toggleClass('glyphicon-chevron-up');
@@ -9,7 +8,6 @@ $(document).ready(function() {
 
   $('.item').click(function(e) {
     $(this).next('tr').toggle('fade');
-
     var icon = $(this).find('span').first();
     icon.toggleClass('glyphicon-triangle-top');
     icon.toggleClass('glyphicon-triangle-bottom');
